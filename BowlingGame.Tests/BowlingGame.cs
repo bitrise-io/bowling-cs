ed03887155799204aacefc2535cc99ad9003c2c6
Imre Kelényi <imre.kelenyi@gmail.com>
2022-07-25 13:35:03 +0200

    basic spare implementation + tests

--- BowlingGame.Tests/BowlingGame.cs
+++ BowlingGame.Tests/BowlingGame.cs
@@ -5,7 +5,31 @@ namespace BowlingGame.Tests;
 public class BowlingGameTests
 {
     [Theory]
-    [InlineData(new[]{ 0, 0, 0, 0, 0, 0 }, 0)]
+    // gutter game
+    [InlineData(new[]{ 
+        0, 0,   // frame 1
+        0, 0,   // frame 2
+        0, 0 }, // frame 3
+        0)]
+    // simple game
+    [InlineData(new[]{ 
+        1, 2,   // frame 1
+        4, 1,   // frame 2
+        5, 3 }, // frame 3
+        1+2 + 4+1 + 5+3)]
+    // spare game
+    [InlineData(new[]{ 
+        1, 2,   // frame 1
+        9, 1,   // frame 2 (spare)
+        5, 3 }, // frame 3
+        1+2 + 1+9+5 + 5+3)]
+    // strike game
+    [InlineData(new[]{ 
+        1, 2,   // frame 1 
+        10,     // frame 2 (strike)
+        5, 3 }, // frame 3
+        1+2 + 10+5+3 + 5+3)]
+        
     public void Score(int[] rolls, int expectedScore) 
     {
         var game = new BowlingGame();
--- BowlingGame/BowlingGame.cs
+++ BowlingGame/BowlingGame.cs
@@ -2,10 +2,26 @@
 
 public class BowlingGame
 {
-    public int Score { get; set; }
+    private List<int> rolls = new List<int>();
+
+    public int Score { 
+        get 
+        {
+            int sum = 0;
+            for (int i=0; i<rolls.Count; i++) {
+                sum += rolls[i];
+
+                if ((i >= 2) && (i%2 == 0) && 
+                    (rolls[i - 2] + rolls[i - 1] == 10))
+                    sum += rolls[i];
+            }
+
+            return sum;
+        } 
+     }
 
     public void Roll(int pinCount)
     {
-        
+        rolls.Add(pinCount);        
     }
 }
