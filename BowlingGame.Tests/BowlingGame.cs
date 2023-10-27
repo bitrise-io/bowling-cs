b194f53aa8e9ff713777ffa051db81761c198791
Gabor Szakacs <gabor.szakacs@bitrise.io>
2023-10-27 09:22:26 +0200

    [WIP] Spare

--- BowlingGame.Tests/BowlingGame.cs
+++ BowlingGame.Tests/BowlingGame.cs
@@ -5,8 +5,21 @@ namespace BowlingGame.Tests;
 public class BowlingGameTests
 {
     [Theory]
-    [InlineData(new[]{ 0, 0, 0, 0, 0, 0 }, 0)]
-    public void Score(int[] rolls, int expectedScore) 
+    // simple game
+    [InlineData(new[]{
+        1, 2,   // frame 1
+        4, 1,   // frame 2
+        5, 3 }, // frame 3
+        1+2 + 4+1 + 5+3)]
+
+    // spare game
+    [InlineData(new[]{
+        1, 2,   // frame 1
+        9, 1,   // frame 2 (spare)
+        5, 3 }, // frame 3
+        1+2 + 1+9+5 + 5+3)]
+
+    public void Score(int[] rolls, int expectedScore)
     {
         var game = new BowlingGame();
 
--- BowlingGame/BowlingGame.cs
+++ BowlingGame/BowlingGame.cs
@@ -2,10 +2,21 @@
 
 public class BowlingGame
 {
-    public int Score { get; set; }
+    private List<int> rolls = new List<int>();
+    public int Score {
+        get
+        {
+            int sum = 0;
+            for (int i=0; i<rolls.Count; i++) {
+                sum += rolls[i];
+            }
+
+            return sum;
+        }
+     }
 
     public void Roll(int pinCount)
     {
-        
+        rolls.Add(pinCount);
     }
 }
